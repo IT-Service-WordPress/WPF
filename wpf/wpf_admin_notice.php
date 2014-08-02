7b5253fb8bd8d9a6981e488f8075a880ddbda6fb
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-02 21:34:01 +0400

    * изменил префикс файлов на `wpf_` * вынес функции для работы с шаблонами отображений в `wpf_templates.php`

---
+++ wpf/wpf_admin_notice.php
@@ -0,0 +1,58 @@
+<?php 
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! defined( 'WPF_DIR' ) ) {
+	define( 'WPF_DIR', __DIR__ );
+};
+
+require_once ( 'wpf_templates.php' );
+
+/*
+WP_admin_notice class. This class should ideally be used to work with the
+administrative side of the WordPress site.
+
+@since 1.0.0
+
+@package   Wordpress plugin framework
+@author    Sergey S. Betke <Sergey.S.Betke@yandex.ru>
+@license   GPL-2.0+
+@copyright 2014 ООО "Инженер-53"
+*/
+class WP_admin_notice {
+
+	protected
+	$message;
+
+	protected
+	$message_type;
+
+	public
+	function display() {
+		$_template_file = wpf_locate_template( 'admin_notice.php' );
+		require_once( $_template_file );
+	}
+	
+	public
+	function __construct (
+		$message
+		, $type = 'updated' // 'updated', 'error', 'update-nag'
+	) {
+		$this->message = $message;
+		$this->message_type = $type;
+		add_action( 'admin_notices', array( &$this, 'display' ) );
+	}
+
+	private
+	function __clone() {}
+
+    private
+	function __sleep() {}
+
+    private
+	function __wakeup() {}
+}
+?>
---
+++ wpf/wpf_templates.php
@@ -0,0 +1,71 @@
+<?php 
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! defined( 'WPF_DIR' ) ) {
+	define( 'WPF_DIR', __DIR__ );
+};
+
+/*
+Functions for plugin templates processing.
+
+@since 1.0.0
+
+@package   Wordpress plugin framework
+@author    Sergey S. Betke <Sergey.S.Betke@yandex.ru>
+@license   GPL-2.0+
+@copyright 2014 ООО "Инженер-53"
+*/
+
+/*
+Locate a template
+
+Allows parent/child themes to override the markup by placing the a file named basename( $default_template_path ) in their root folder,
+and also allows plugins or themes to override the markup by a filter. Themes might prefer that method if they place their templates
+in sub-directories to avoid cluttering the root folder. In both cases, the theme/plugin will have access to the variables so they can
+fully customize the output.
+
+@since 1.0.0
+
+@mvc @model
+
+@param  string $default_template_path The path to the template, relative to the plugin's `views` folder
+@param  array  $variables             An array of variables to pass into the template's scope, indexed with the variable name so that it can be extract()-ed
+@param  string $require               'once' to use require_once() | 'always' to use require()
+@return string						  located templates
+*/
+function wpf_locate_template(
+	$template_names
+	, $plugin_file = __FILE__
+	, $load = false
+	, $require_once = true
+) {
+	$located = '';
+	$located = locate_template( $template_names );
+	
+	if ( !$located ) {
+		foreach ( (array) $template_names as $template_name ) {
+			if ( !$template_name )
+				continue;
+			$test_path = dirname( $plugin_file ) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $template_name;
+			if ( file_exists( $test_path ) ) {
+				$located = $test_path;
+				break;
+			}
+			$test_path = WPF_DIR . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $template_name;
+			if ( file_exists( $test_path ) ) {
+				$located = $test_path;
+				break;
+			}
+		}
+	}
+	if ( $load && '' != $located )
+		load_template( $located, $require_once );
+
+	return $located;
+}
+
+?>
