3ddef9fc9e1969cf58036ebff6bfab69dea8b478
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-07 15:55:45 +0400

    * вынес функционал проверки зависимостей в отдельный базовый класс для компонентов (#16)

--- wpf/wpf_plugin_component_activable.php
+++ wpf/wpf_plugin_component_activable.php
@@ -4,6 +4,7 @@ namespace WPF\v1\Plugin\Component;
 
 require_once ( 'wpf_inc.php' );
 require_once ( 'wpf_plugin_component_base.php' );
+require_once ( 'wpf_plugin_component_dependable.php' );
 require_once ( 'wpf_plugin_component_iactivable.php' );
 
 /*
@@ -19,34 +20,14 @@ Activable components base class.
 abstract
 class Activable
 	extends
-		Base
+		Dependable
 	implements
 		IActivable
 {
 
-	public
-	function bind_action_handlers_and_filters() {
-		if ( \WP_DEBUG ) {
-			if ( \is_admin() /* && \current_user_can( 'install_plugins' ) */ ) {
-				$this->plugin->add_action( 'admin_init', array( $this, 'check_dependencies' ) ); 
-			};
-		};
-	}
-
-	public
-	function check_dependencies() {
-		if ( ! $this->plugin->has_component( '\WPF\v1\Plugin\Component\IActivator' ) ) {
-			require_once( 'wpf_gui_notice_admin.php' );
-			new \WPF\v1\GUI\Notice\Admin(
-				sprintf(
-					__( 'Plugin "%1$s" coding error: component <code>%2$s</code> requires <code>%3$s</code> component in plugin.', \WPF\v1\WPF_ADMINTEXTDOMAIN )
-					, $this->plugin->get_title()
-					, \get_class( $this )
-					, 'IActivator'
-				)
-				, 'error'
-			);
-		};
+	protected
+	function get_dependencies() {
+		return array( '\WPF\v1\Plugin\Component\IActivator' );
 	}
 
 }
---
+++ wpf/wpf_plugin_component_dependable.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace WPF\v1\Plugin\Component;
+
+require_once ( 'wpf_inc.php' );
+require_once ( 'wpf_plugin_component_base.php' );
+require_once ( 'wpf_plugin_component_iactivable.php' );
+
+/*
+Components with dependencies base class.
+
+@since 1.0.0
+
+@package   Wordpress plugin framework
+@author    Sergey S. Betke <Sergey.S.Betke@yandex.ru>
+@license   GPL-2.0+
+@copyright 2014 ООО "Инженер-53"
+*/
+abstract
+class Dependable
+	extends
+		Base
+{
+
+	public
+	function bind_action_handlers_and_filters() {
+		if ( \WP_DEBUG ) {
+			if ( \is_admin() /* && \current_user_can( 'install_plugins' ) */ ) {
+				$this->plugin->add_action( 'admin_init', array( $this, 'check_dependencies' ) ); 
+			};
+		};
+	}
+
+	abstract
+	protected
+	// array
+	function get_dependencies();
+
+	public
+	function check_dependencies() {
+		foreach ( $this->get_dependencies() as $dependency ) {
+			if ( ! $this->plugin->has_component( $dependency ) ) {
+				require_once( 'wpf_gui_notice_admin.php' );
+				new \WPF\v1\GUI\Notice\Admin(
+					sprintf(
+						__( 'Plugin "%1$s" coding error: component <code>%2$s</code> requires <code>%3$s</code> component in plugin.', \WPF\v1\WPF_ADMINTEXTDOMAIN )
+						, $this->plugin->get_title()
+						, \get_class( $this )
+						, 'IActivator'
+					)
+					, 'error'
+				);
+			};
+		};
+	}
+
+}
+?>
