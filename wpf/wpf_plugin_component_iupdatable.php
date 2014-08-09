cd07eeeef7baacb745e9bd296f11865e6a5e01de
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-09 19:57:32 +0400

    + добавил новый базовый интерфейс для устанавливаемых и обновляемых компонентов - `wpf/wpf_plugin_component_iupdatable.php` (#16)

---
+++ wpf/wpf_plugin_component_iupdatable.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace WPF\v1\Plugin\Component;
+
+require_once ( 'wpf_inc.php' );
+require_once ( 'wpf_plugin_component_iinstallabe.php' );
+
+/*
+
+@since 1.0.0
+
+@package   Wordpress plugin framework
+@author    Sergey S. Betke <Sergey.S.Betke@yandex.ru>
+@license   GPL-2.0+
+@copyright 2014 ООО "Инженер-53"
+*/
+interface IUpdatable
+	extends
+		IInstallable
+{
+
+	public
+	function update(
+		$from_version
+	);
+
+}
+?>
