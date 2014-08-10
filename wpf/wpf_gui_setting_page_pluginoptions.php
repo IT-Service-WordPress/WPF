8367bca04993e1cb5a8dcc243dc8ede96028b6c0
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-10 15:46:34 +0400

    + в списке плагинов добавил ссылку на страницу настроек плагина (#20)

--- wpf/wpf_gui_setting_page_pluginoptions.php
+++ wpf/wpf_gui_setting_page_pluginoptions.php
@@ -27,6 +27,45 @@ class PluginOptions
 	}
 
 	public
+	function bind_action_handlers_and_filters() {
+		parent::bind_action_handlers_and_filters();
+		\add_action( 'pre_current_active_plugins', array( &$this, '_add_settings_page_link' ) ); 
+	}
+
+	final
+	public
+	function _add_settings_page_link() {
+		\add_filter( 'plugin_action_links_' . $this->plugin->get_basename(), array( &$this, 'add_settings_page_link' ) ); 
+	}
+
+	/** 
+	 * Add settings link to plugin list table 
+	 * @param  array $links Existing links 
+	 * @return array 		Modified links 
+	 */ 
+	public
+	function add_settings_page_link(
+		$links
+	) { 
+		$settings_link =
+			'<a href="'
+				. add_query_arg(
+					'page'
+					, $this->plugin->get_slug()
+					, \self_admin_url( 'options-general.php' )
+				)
+			. '"' 
+			. ' title="' . esc_attr__( 'Settings' )	. '"'
+			. ' class="edit"'
+			. '>'
+			. __( 'Settings' )
+			. '</a>'
+		;
+		array_push( $links, $settings_link ); 
+		return $links; 
+	}
+
+	public
 	function get_page_title() {
 		return $this->plugin->get_title( false );
 	}
