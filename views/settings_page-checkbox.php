904928100cf5c4e06bfb2efc02869dd66fcbbdfb
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-21 15:33:20 +0400

    + добавил элемент управления для страниц настроек - одиночный checkbox

---
+++ views/settings_page-checkbox.php
@@ -0,0 +1,6 @@
+<fieldset>
+	<input id="<?php echo esc_attr( $this->get_id() ); ?>" name="<?php echo esc_attr( $this->get_option_name() ); ?>" type="checkbox" <?php if ( $this->get_option_value() ) { ?>checked="checked" <?php }; ?>value="true"></input>
+	<?php if ( $this->get_description() ) { ?>
+		<p class="description"><?php echo $this->get_description(); ?></p>
+	<?php }; ?>
+</fieldset>
---
+++ wpf_gui_setting_page_control_checkbox.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace WPF\v1\GUI\Setting\Page\Control;
+
+require_once ( 'wpf_inc.php' );
+require_once ( 'wpf_gui_setting_page_control_base.php' );
+require_once ( 'wpf_gui_templates.php' );
+
+/*
+Checkbox control class (for settings page).
+
+@since 1.0.0
+
+@package   Wordpress plugin framework
+@author    Sergey S. Betke <Sergey.S.Betke@yandex.ru>
+@license   GPL-2.0+
+@copyright 2014 ООО "Инженер-53"
+*/
+class Checkbox
+	extends
+		Base
+{
+
+	public
+	function display() {
+		$_template_file = \WPF\v1\GUI\locate_template( 'settings_page-checkbox.php' );
+		require( $_template_file );
+	}
+
+}
+?>
