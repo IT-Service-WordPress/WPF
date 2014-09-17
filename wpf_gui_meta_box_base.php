04497de9392398cca7df7eb0d390690bfe2ca1e6
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-09-18 00:33:25 +0400

    + сообщения об ошибках при сохранении метаданных (#53) уже вполне могу генерировать (#44).

--- wpf_gui_meta_box_base.php
+++ wpf_gui_meta_box_base.php
@@ -4,8 +4,8 @@ namespace WPF\v1\GUI\Meta\Box;
 
 require_once ( 'wpf_gui_meta_box_ibase.php' );
 require_once ( 'wpf_gui_group_controller.php' );
-require_once ( 'wpf_plugin_component_base.php' );
 require_once ( 'wpf_gui_templates.php' );
+require_once ( 'wpf_plugin_component_base.php' );
 require_once ( 'wpf_functions.php' );
 
 /*
@@ -74,7 +74,7 @@ class Base
 	public
 	function bind_action_handlers_and_filters() {
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
-		add_action( 'save_post', array( $this, 'save_meta' ) );
+		add_action( 'save_post_' . $this->get_post_type(), array( $this, 'save_meta' ) );
 		add_action(
 			'load-' . $this->get_page_hookname()
 			, array( &$this, 'on_page_load' )
@@ -118,40 +118,6 @@ class Base
 		);
 	}
 
-	protected
-	function do_metas_fields() {
-		foreach ( $this->get_controls() as $control ) {
-			$data_manipulator = $control;
-			/*
-			\register_setting(
-				$this->get_option_group()
-				, $data_manipulator->get_name()
-				, function (
-					$new_value
-				) use (
-					$data_manipulator
-					, $plugin
-				) {
-					if ( $validator = $data_manipulator->get_validator() ) {
-						$new_value = call_user_func( $validator->get_callback(), $new_value );
-					};
-					$option = $plugin->get_options()->get( $data_manipulator->get_name() );
-					$new_value = $option->sanitize_value( $new_value );
-					return $new_value;
-				}
-			);
-			\add_settings_field(
-				$control->get_id()
-				, $control->get_label()
-				, array( $control, 'display' )
-				, $this->get_page_slug()
-				, $section->get_id()
-				, array( 'label_for' => $control->get_id() )
-			);
-			*/
-		};
-	}
-
 	public
 	function display() {
 		$_template_file = \WPF\v1\GUI\locate_template( 'meta_box.php' );
@@ -159,22 +125,26 @@ class Base
 	}
 
 	public
-	function save_meta(
-		$post_id
-	) {
-		/*
-		// metas ...
-		if ( ! isset( $_POST[ 'myplugin_new_field' ] ) ) return;
-		$my_data = sanitize_text_field( $_POST[ 'myplugin_new_field' ] );
-		update_post_meta( $post_id, '_my_meta_value_key', $my_data );
-		*/
+	function save_meta() {
+		foreach ( $this->get_controls() as $data_manipulator ) {
+			if ( isset( $_POST[ $name = $data_manipulator->get_name() ] ) ) {
+				$new_value = $_POST[ $name ];
+				$new_value = sanitize_text_field( $new_value );
+				if ( $validator = $data_manipulator->get_validator() ) {
+					$new_value = call_user_func( $validator->get_callback(), $new_value );
+				};
+				// $new_value = $this->get_plugin()->get_metas()->get( $name )->sanitize_value( $new_value );
+				$this->set_value( $data_manipulator->get_name(), $new_value );
+				// update_post_meta( $post_id, '_my_meta_value_key', $my_data );
+			};
+		};
 	}
 
 	public
 	function get_value(
 		$name
 	) {
-		// return $this->get_plugin()->get_options()->$name;
+		// return $this->get_plugin()->get_metas()->$name;
 	}
 
 	public
@@ -182,21 +152,21 @@ class Base
 		$name
 		, $new_value
 	) {
-		// return $this->get_plugin()->get_options()->$name = $new_value;
+		// return $this->get_plugin()->get_metas()->$name = $new_value;
 	}
 
 	public
 	function unset_value(
 		$name
 	) {
-		// return $this->get_plugin()->get_options()->get( $name )->unset_value();
+		// return $this->get_plugin()->get_metas()->get( $name )->unset_value();
 	}
 
 	public
 	function isset_value(
 		$name
 	) {
-		// return $this->get_plugin()->get_options()->get( $name )->isset_value();
+		// return $this->get_plugin()->get_metas()->get( $name )->isset_value();
 	}
 
 	public
@@ -205,14 +175,13 @@ class Base
 		, $message
 		, $code = 'updated'
 	) {
-		/*
-		\add_settings_error(
-			$name
-			, 'settings_updated'
-			, $message
-			, $code
+		require_once ( 'wpf_gui_notice_scheduled.php' );
+		$this->get_plugin()->add_components(
+			new \WPF\v1\GUI\Notice\Scheduled( array(
+				'message' => $message
+				, 'message_type' => $code
+			) )
 		);
-		*/
 	}
 
 }
